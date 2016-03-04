3d1ba82a9d6451e52e5cffa33393c7ba10a9bfb3
phoebe <phoebe.greig@gmail.com>
2016-03-04 15:22:28 +0000

    ch21

--- lib/van.rb
+++ lib/van.rb
@@ -3,18 +3,26 @@ require_relative 'Docking_Station'
 
 class Van
 
-attr_reader :van_storage
-
-  def initialize
+attr_reader :van_storage, :capacity
+DEFAULT_CAPACITY = 10
+  def initialize(capacity = DEFAULT_CAPACITY)
     @van_storage = []
+    @capacity = capacity
   end
-
-
 
   def loading(from)
     if from.class == Garage
-      from.storage.each { |bike| van_storage << bike if bike.working? }
-      from.storage.clear
+
+      please_work = from.storage
+      help(please_work)
+      # from.storage.each do |bike| 
+      # fail 'This van is now full' if from.storage.length == capacity 
+      #   if bike.working? 
+      #     van_storage << bike  
+      #     from.storage.delete(bike) 
+      #   end      
+      # end
+      
     else
       from.bikes.each {|bike| van_storage << bike if bike.working? == false }
       from.bikes.delete {|bike| bike.working? == false}
@@ -26,5 +34,18 @@ attr_reader :van_storage
     van_storage.each {|bike| to << bike }
     van_storage.clear
   end
+
+private
+# please_work = from.storage
+  def help(please_work)
+    please_work.each do |bike| 
+    fail 'This van is now full' if please_work.length == capacity 
+        if bike.working? 
+          van_storage << bike  
+          please_work.delete(bike) 
+        end  
+    end
+  end
+
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -18,7 +18,7 @@ describe Van do
 	it 'unloads all bikes from van to garage' do
 		garage = Garage.new
 		3.times {subject.van_storage << Bike.new}
-		subject.unload(garage)
+		subject.unload(garage.storage)
 		expect(garage.storage.length).to eq 3
 		expect(subject.van_storage).to eq []
 	end
